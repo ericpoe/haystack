fbbaf0bdd55aac94ef4f70b670697885dcca452d
Eric Poe <eric.poe@gmail.com>
2015-06-30 08:08:10 -0500

    Update docblock

--- lib/BaseFunctional.php
+++ lib/BaseFunctional.php
@@ -3,13 +3,19 @@ namespace OPHP;
 
 interface BaseFunctional
 {
+    /**
+     * Applies the callback to the elements of the given array
+     *
+     * @param callable $func
+     * @return mixed
+     */
     public function map(callable $func);
 
     /**
      * Walk does an in-place update of items in the object.
      *
-     * Since the update is in-place, this breaks the immutablity of OPHP objects. This is useful for very large implementations
-     * of the OPHP where cloning the object would be memory intensive.
+     * Since the update is in-place, this breaks the immutablity of OPHP objects. This is useful for very large
+     * implementation of the OPHP where cloning the object would be memory intensive.
      *
      * @param callable $func
      * @return bool
