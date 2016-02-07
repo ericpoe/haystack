fee04c7ce76dcdcc8f7329040efcf5b6a39d319b
Eric Poe <eric.poe@gmail.com>
2016-02-07 12:04:24 -0600

    Move locate tests to their own classes

--- tests/OArrayTest.php
+++ tests/OArrayTest.php
@@ -76,60 +76,6 @@ class OArrayTest extends \PHPUnit_Framework_TestCase
     }
 
     /**
-     * @dataProvider arrayLocateProvider
-     *
-     * @param $type
-     * @param $checkThing
-     * @param $expected
-     */
-    public function testLocateStringTypeInOArray($type, $checkThing, $expected)
-    {
-        if ("list" === $type) {
-            $var = $this->arrList->locate($checkThing);
-        } else {
-            $var = $this->arrDict->locate($checkThing);
-        }
-
-        $this->assertEquals($expected, $var);
-    }
-
-    public function arrayLocateProvider()
-    {
-        return [
-            "1st item in list" => ["list", "apple", 0],
-            "String not in list" => ["list", "fobble", -1],
-            "1st OString in list" => ["list", new OString("apple"), 0],
-            "OString not in list" => ["list", new OString("fobble"), -1],
-            "1st item in dictionary" => ["dict", "apple", 'a'],
-            "String not in dictionary" => ["dict", "fobble", -1],
-            "1st OString in dictionary" => ["dict", new OString("apple"), 'a'],
-            "OString not in dictionary" => ["dict", new OString("fobble"), -1],
-        ];
-    }
-
-    /**
-     * @dataProvider badArrayContainsProvider
-     *
-     * @param $item
-     * @param $exceptionMsg
-     */
-    public function testLocateBadThingsInOArray($item, $exceptionMsg)
-    {
-        $this->expectException("InvalidArgumentException");
-        $this->expectExceptionMessage($exceptionMsg);
-
-        $this->arrList->locate($item);
-    }
-
-    public function badArrayContainsProvider()
-    {
-        return [
-            "DateTime" => [new \DateTime(), "DateTime cannot be contained within an OArray"],
-            "SPL Object" => [new \SplDoublyLinkedList(), "SplDoublyLinkedList cannot be contained within an OArray"],
-        ];
-    }
-
-    /**
      * @dataProvider appendProvider
      *
      * @param $type
--- tests/OStringTest.php
+++ tests/OStringTest.php
@@ -75,53 +75,6 @@ class OStringTest extends \PHPUnit_Framework_TestCase
     }
 
     /**
-     * @dataProvider stringLocateProvider()
-     *
-     * @param $checkString
-     * @param $expected
-     */
-    public function testLocateTypesOfStringInFoobar($checkString, $expected)
-    {
-        $var = $this->aString->locate($checkString);
-        $this->assertEquals($expected, $var);
-    }
-
-    public function stringLocateProvider()
-    {
-        return [
-            "String known-present" => ["oob", 1],
-            "String known-missing" => ["baz", -1],
-            "OString known-present" => [new OString('oob'), 1],
-            "OString known-missing" => [new OString('baz'), -1],
-            "Integer known-missing" => [42, -1],
-            "OString integer known-missing" => [new OString(42), -1],
-
-        ];
-    }
-
-    /**
-     * @dataProvider badLocateTypesOfStringInFoobarProvider
-     * @param $item
-     * @param $exceptionMsg
-     * @throws \InvalidArgumentException
-     */
-    public function testBadLocateTypesOfStringInFoobar($item, $exceptionMsg)
-    {
-        $this->expectException("InvalidArgumentException");
-        $this->expectExceptionMessage($exceptionMsg);
-
-        $var = $this->aString->locate($item);
-    }
-
-    public function badLocateTypesOfStringInFoobarProvider()
-    {
-        return [
-            "DateTime" => [new \DateTime(), "DateTime is neither a scalar value nor an OString"],
-            "SplDoublyLinkedList" => [new \SplDoublyLinkedList(), "SplDoublyLinkedList is neither a scalar value nor an OString"],
-        ];
-    }
-
-    /**
      * @dataProvider stringAppendProvider()
      *
      * @param $babyString
