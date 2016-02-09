121fecad3fe94dad9faa341e588e947abb18cf0c
Eric Poe <eric.poe@gmail.com>
2016-02-08 21:37:26 -0600

    Move OArray::slice to its own class

--- src/OArray.php
+++ src/OArray.php
@@ -136,18 +136,8 @@ class OArray extends \ArrayObject implements ContainerInterface, BaseFunctionalI
      */
     public function slice($start, $length = null)
     {
-        if (is_null($start) || !is_numeric($start)) {
-            throw new \InvalidArgumentException("Slice parameter 1, \$start, must be an integer");
-        }
-
-        if (!is_null($length) && !is_numeric($length)) {
-            throw new \InvalidArgumentException("Slice parameter 2, \$length, must be null or an integer");
-        }
-
-        $maintainIndices = false;
-
-        return new OArray(array_slice($this->arr, $start, $length, $maintainIndices));
-
+        $answer = new OArraySlice($this);
+        return new OArray($answer->slice($start, $length));
     }
 
     /**
--- tests/OArrayTest.php
+++ tests/OArrayTest.php
@@ -6,9 +6,9 @@ use OPHP\OString;
 
 class OArrayTest extends \PHPUnit_Framework_TestCase
 {
-    /** @var  OArray */
+    /** @var OArray */
     private $arrList;
-    /** @var  OArray */
+    /** @var OArray */
     private $arrDict;
 
     protected function setUp()
@@ -72,118 +72,6 @@ class OArrayTest extends \PHPUnit_Framework_TestCase
         return [
             "DateTime" => [new \DateTime(), "DateTime cannot be instantiated as an OArray"],
             "SPL Object" => [new \SplDoublyLinkedList(), "SplDoublyLinkedList cannot be instantiated as an OArray"],
-        ];
-    }
-
-    /**
-     * @dataProvider firstPartOfArraySliceProvider
-     *
-     * @param $type
-     * @param $expected
-     */
-    public function testGetFirstPartOfTypesOfArrayUsingSlice($type, $expected)
-    {
-        if ("list" === $type) {
-            $subArray = $this->arrList->slice(0, 2);
-        } else {
-            $subArray = $this->arrDict->slice(0, 2);
-        }
-
-        $this->assertEquals($expected, $subArray);
-    }
-
-    public function firstPartOfArraySliceProvider()
-    {
-        return [
-            "First two items of list" => ["list", new OArray(["apple", "bobble"])],
-            "First two items of dictionary" => ["dict", new OArray(["a" => "apple", "b" => "bobble"])],
-        ];
-    }
-
-    /**
-     * @dataProvider lastPartOfArraySliceProvider
-     *
-     * @param $type
-     * @param $expected
-     */
-    public function testGetLastPartOfTypesOfArrayUsingSlice($type, $expected)
-    {
-        if ("list" === $type) {
-            $subArray = $this->arrList->slice(-2);
-        } else {
-            $subArray = $this->arrDict->slice(-2);
-        }
-
-        $this->assertEquals($expected, $subArray);
-    }
-
-    public function lastPartOfArraySliceProvider()
-    {
-        return [
-            "Last two items of list" => ["list", new OArray(["cobble", "dobble"])],
-            "Last two items of dictionary" => ["dict", new OArray(["c" => "cobble", "d" => "dobble"])],
-        ];
-    }
-
-    /**
-     * @dataProvider middlePartOfArraySliceProvider
-     *
-     * @param $type
-     * @param $start
-     * @param $length
-     * @param $expected
-     */
-    public function testGetMiddlePartOfTypesOfArrayUsingSlice($type, $start, $length, $expected)
-    {
-        if ("list" === $type) {
-            $subArray = $this->arrList->slice($start, $length);
-        } else {
-            $subArray = $this->arrDict->slice($start, $length);
-        }
-
-        $this->assertEquals($expected, $subArray);
-    }
-
-    public function middlePartOfArraySliceProvider()
-    {
-        return [
-            "List: Start -3, length: -1" => ["list", "-3", "-1", new OArray(["bobble", "cobble"])],
-            "List: Start 1, length: -1" => ["list", "1", "-1", new OArray(["bobble", "cobble"])],
-            "List: Start 1, length: 2" => ["list", "1", "2", new OArray(["bobble", "cobble"])],
-            "List: Start 1, length: null" => ["list", "1", null, new OArray(["bobble", "cobble", "dobble"])],
-            "Dictionary: Start -3, length: -1" => ["dict", "-3", "-1", new OArray(["b" => "bobble", "c" => "cobble"])],
-            "Dictionary: Start 1, length: -1" => ["dict", "1", "-1", new OArray(["b" => "bobble", "c" => "cobble"])],
-            "Dictionary: Start 1, length: 2" => ["dict", "1", "2", new OArray(["b" => "bobble", "c" => "cobble"])],
-            "Dictionary: Start 1, length: null" => ["dict", "1", null, new OArray(["b" => "bobble", "c" => "cobble", "d" => "dobble"])],
-        ];
-    }
-
-    /**
-     * @dataProvider badArraySliceProvider
-     * @param $type
-     * @param $start
-     * @param $length
-     * @param $exceptionMsg
-     */
-    public function testBadArraySlice($type, $start, $length, $exceptionMsg)
-    {
-        $this->expectException("InvalidArgumentException");
-        $this->expectExceptionMessage($exceptionMsg);
-
-        if ("list" === $type) {
-            $subArray = $this->arrList->slice($start, $length);
-        } else {
-            $subArray = $this->arrDict->slice($start, $length);
-        }
-    }
-
-    public function badArraySliceProvider()
-    {
-        return [
-            "List: non-integer start" => ["list", "b", "2", 'Slice parameter 1, $start, must be an integer'],
-            "Dictionary: non-integer start" => ["dict", "b", "2", 'Slice parameter 1, $start, must be an integer'],
-            "List: non-integer length" => ["list", "1", "b", 'Slice parameter 2, $length, must be null or an integer'],
-            "Dictionary: non-integer length" => ["dict", "1", "b", 'Slice parameter 2, $length, must be null or an integer'],
         ];
     }
 
