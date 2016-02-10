16c71b07e3b9699d1324947829196b037f4ae7e1
Eric Poe <eric.poe@gmail.com>
2016-02-09 20:56:16 -0600

    Add missing docblock

--- src/OArrayMap.php
+++ src/OArrayMap.php
@@ -3,13 +3,21 @@ namespace OPHP;
 
 class OArrayMap
 {
+    /** @var array */
     private $arr;
 
+    /**
+     * @param OArray $array
+     */
     public function __construct(OArray $array)
     {
         $this->arr = $array->toArray();
     }
 
+    /**
+     * @param callable $func
+     * @return array
+     */
     public function map(callable $func)
     {
         return array_map($func, $this->arr);
