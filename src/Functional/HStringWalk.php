221cea182e142901eaaede5194ec99f8844abeb3
Eric Poe <eric.poe@gmail.com>
2019-01-22 12:02:33 -0600

    Remove unused var

--- src/Functional/HStringWalk.php
+++ src/Functional/HStringWalk.php
@@ -5,14 +5,8 @@ use Haystack\HString;
 
 class HStringWalk
 {
-    /**
-     * @param HString &$hString
-     * @param callable $func
-     */
     public static function walk(HString $hString, callable $func)
     {
-        $size = $hString->count();
-
         foreach ($hString as $i => $iValue) {
             $hString[$i] = $func($hString[$i], $i);
         }
